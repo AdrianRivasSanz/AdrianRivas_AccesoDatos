e500cdc2267e4f500b7150ca1f3902bbe93ac97e
Adrian <adrianrivassanz@gmail.com>
2022-12-16 14:09:51 +0100

    update

---
+++ TEMA 3/ListaEnlazada/main.py
@@ -0,0 +1,58 @@
+# EJERCICIO LISTAS ENLAZADAS
+# Crear_nodo
+# Insertar_nodo
+# Buscar_nodo
+# Eliminar_nodo
+
+# 1. CREAR NODO
+def crear_nodo():
+    lista = []
+    numero = int(input("Dame un numero\n"));
+    nodo = [0, numero]
+    return nodo
+
+# 2. INSERTAR NODO
+def insertar_nodo(lista):
+
+
+
+# 3. BUSCAR NODO
+def buscar_nodo():
+
+
+# 4. ELIMINAR NODO
+def eliminar_nodo():
+
+
+# Menu
+def menu():
+    print("¿Que quieres hacer?")
+    print("1. Crear nodo")
+    print("2. Insertar nodo")
+    print("3. Buscar nodo")
+    print("4. Eliminar nodo")
+    print("5. Salir")
+    elegir = int(input())
+    return elegir
+
+
+# Inicio
+def inicio():
+    while True:
+        opcion = menu()
+        if opcion == 1:
+            def crear_nodo(nodo, lista)
+        elif opcion == 2:
+            def insertar_nodo()
+        elif opcion == 3:
+            def buscar_nodo()
+        elif opcion == 4:
+            def eliminar_nodo()
+        elif opcion == 5:
+            print("Salir")
+            break
+        else:
+            print("Opcion invalida")
+
+
+inicio()
