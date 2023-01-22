13189d88d452cca730634751d85da6fb5645bf97
Adrian <117822690+AdrianRivasSanz@users.noreply.github.com>
2023-01-22 20:52:42 +0100

    update

--- TEMA 4/DatosRecursivos.py
+++ TEMA 4/DatosRecursivos.py
@@ -1,104 +1,133 @@
+'''opcion es para elegir entre forma iterativa o recursiva
+   opc es para elegir las opciones del menu'''
+
 while True:
-    print("1. Recursivo  2. Iterativo")
-    opc = input()
-    
-    if opc == "1":
-        print("1.Factorial  2.Multiplicar  3.MCD   4.Exponente  5.Resta")
-        opc2 = input()
-        
-        if opc2 == "1":
+    print("1. Forma_Iterativa  2. Forma_Recursiva")
+    opcion = input()
+
+    # FORMA ITERATIVA
+    if opcion == "1":
+        print("1.FACTORIAL  2.MULTIPLICACION  3.MCD   4.EXPONENTE  5.RESTA")
+        opc = input()
+
+        #1.FACTORIAL
+        if opc == "1":
+
+            def factorial(num):
+                resultado = 1
+                if num > 1:
+                    for i in range(1, num + 1):
+                        resultado = resultado * i
+                    return print(resultado)
+
+            factorial()
+
+
+        #2.MULTILICACION
+        if opc == "2":
+
+            def multmayor0(num1, num2):
+                return num1 * num2
+
+            multmayor0(2, 5)
+
+
+        #3.MCD
+        if opc == "3":
+
+            def mcd(num1, num2):
+                contador = 0
+                while num2 != 0:
+                    contador = num2
+                    num2 = num1 % num2
+                    num1 = contador
+                return num1
+
+            mcd(7, 14)
+
+
+        #4.EXPONENTE 
+        if opc == "4":
+
+            def potencia(num1, num2):
+                return num1 ** num2
+
+            potencia(2, 3)
+
+
+        #5.RESTA
+        if opc == "5":
+
+            def resta(num1, num2):
+
+                return num1 - num2
+
+            resta(5, 2)
+
+
+
+    # FORMA RECURSIVA
+    if opcion == "2":
+        print("1.FACTORIAL  2.MULTIPLICACION  3.MCD   4.EXPONENTE  5.RESTA")
+        opc = input()
+
+        #1.FACTORIAL
+        if opc == "1":
+
             def factorial(num):
                 if num == 1:
                     return 1
                 else:
-                    return num*factorial(num - 1)                
+                    return num * factorial(num - 1)
+
             print(factorial(5))
-        
-        if opc2 == "2":
 
-            def multmayor0(op1, op2):
-                
-                if op1 == 1:
-                    return op2
-                elif op2 == 1:
-                    return op1
+
+        #2.MULTIPLICACION
+        if opc == "2":
+
+            def multmayor0(num1, num2):
+
+                if num1 == 1:
+                    return num2
+                elif num2 == 1:
+                    return num1
                 else:
-                    return op1 + multmayor0(op1,op2 -1)
-            
-            print(multmayor0(2,3))
-        
-        if opc2 == "3":
-            def mcd(op1, op2):
+                    return num1 + multmayor0(num1, num2 - 1)
 
-                if op2 == 0:
-                    return op1
-                return mcd(op2, op1 % op2)
+            print(multmayor0(2, 5))
 
-            print(mcd(13,26))
-            
-        if opc2 == "4":
-            def potencia(op1, op2):
-                if(op2 == 0):
+
+        #3.MCD
+        if opc == "3":
+
+            def mcd(num1, num2):
+                if num2 == 0:
+                    return num1
+                return mcd(num2, num1 % num2)
+
+            print(mcd(7, 14))
+
+
+        #4.EXPONENTE
+        if opc == "4":
+
+            def potencia(num1, num2):
+                if num2 == 0:
                     return 1
                 else:
-                    return op1 * potencia(op1,op2 - 1)    
-            print(potencia(2,3))
-            
-        if opc2 == "5":
-            def resta(op1, op2):
-                if op1 == 0:
-                    return op1
-                else:
-                    return resta(op1, op2 - 1) - 1
-            print(resta(4,2))
-       
-    
-    if opc == "2":
-        print("1.Factorial  2.Multiplicar  3.MCD   4.Exponente  5.Resta")
-        opc2 = input()
-        
-        if opc2 == "1":
-            def factorial(num):
-                resultado = 1
-                if num > 1:
-                    for i in range(1, num+1):
-                        resultado = resultado * i
-                    
-                    return print(resultado)
-            factorial(5)
-        
-        
-        if opc2 == "2":
-            def multmayor0(op1, op2):
-            
-                return op1 * op2
-            
-            multmayor0(2,3)
-        
-        
-        if opc2 == "3":
-            def mcd(op1, op2):
-                 contador = 0
-                 while op2 != 0:
-                    contador = op2
-                    op2 = op1 % op2
-                    op1 = contador
-                 return op1
+                    return num1 * potencia(num1, num2 - 1)
 
-            mcd(13,26)
-            
-            
-            
-        if opc2 =="4":
-            def potencia(op1, op2):
-                
-                return op1 ** op2
-            
-            potencia(2,3)
-        
-        
-        if opc2 == "5":
-            def resta(op1,op2):
-                
-                return op1 - op2
-            resta(4,2)
+            print(potencia(2, 3))
+
+
+        #5.RESTA
+        if opc == "5":
+
+            def resta(num1, num2):
+                if num1 == 0:
+                    return num1
+                else:
+                    return resta(num1, num2 - 1) - 1
+
+            print(resta(3, 2))
