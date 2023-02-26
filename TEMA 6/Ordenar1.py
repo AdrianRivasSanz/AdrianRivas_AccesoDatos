e3d612794263c58f2f3ba85fad2ca27badcee49e
Adrian <117822690+AdrianRivasSanz@users.noreply.github.com>
2023-02-26 14:04:52 +0100

    update

--- TEMA 6/Ordenar1.py
+++ TEMA 6/Ordenar1.py
@@ -9,3 +9,18 @@ def ordenar1(lista):
                 listaNumeros[j+1] = aux
 
     print(listaNumeros)
+    
+    
+#SOLUCION LUISMI
+def ord_burbuja(array):
+    n = len(array)
+
+    for i in range(n-1):       # <-- bucle padre
+        for j in range(n-1-i): # <-- bucle hijo
+            if array[j] > array[j+1]:
+                array[j], array[j+1] = array[j+1], array[j]
+
+elementos = [8, 3, 1, 19, 14]
+ord_burbuja(elementos)
+
+print(elementos)
